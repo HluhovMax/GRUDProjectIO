cad9e4bef2d3e5a865eb3906c7f419a4943e365c
Max Hluhov <holosievo@gmail.com>
2018-10-14 17:03:01 +0300

    'v'

--- src/main/java/mvc/model/Skill.java
+++ src/main/java/mvc/model/Skill.java
@@ -18,15 +18,7 @@ public class Skill implements Serializable{
         return ID;
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
