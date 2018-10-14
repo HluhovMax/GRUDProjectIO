394a437b83fa24699dc0116aa6a2bdb1947f3e39
Max Hluhov <holosievo@gmail.com>
2018-10-14 17:56:41 +0300

    'b'

--- src/main/java/mvc/repository/JavaIOSkillRepositoryImpl.java
+++ src/main/java/mvc/repository/JavaIOSkillRepositoryImpl.java
@@ -10,14 +10,10 @@ import java.io.*;
 public class JavaIOSkillRepositoryImpl implements SkillRepository {
     @Override
     public void save(Skill skill) throws IOException {
-        String ID = skill.getID();
-        String name = skill.getName();
-        byte[] buff = name.getBytes();
-        byte[] idBuff = ID.getBytes();
         File file = new File("src\\main\\resources\\files\\skills.txt");
         try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write(ID);
-            fw.write(name + "\n");
+            fw.write(skill.getID());
+            fw.write(skill.getName() + "\n");
         } catch (IOException e) {
             System.out.println("IO exception: " + e);
         }
