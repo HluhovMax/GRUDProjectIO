837deefc9293316752629d2a83cc2b4e4cc02ec8
Max Hluhov <holosievo@gmail.com>
2018-10-15 22:04:00 +0300

    'n'

--- src/main/java/mvc/AppRunner.java
+++ src/main/java/mvc/AppRunner.java
@@ -1,13 +1,16 @@
 package mvc;
 
+import mvc.repository.JavaIOSkillRepositoryImpl;
+
 import java.io.IOException;
 
 /**
+ * 1.java/2.css/3.html/4.c++/
  * Created by Max Hluhov on 14.10.2018.
  */
 public class AppRunner {
     public static void main(String[] args) throws IOException {
-        //Runner.oneTimeAddingSkill();
-        Runner.howManySkillsDoYouWantToAdd(1);
+        JavaIOSkillRepositoryImpl r = new JavaIOSkillRepositoryImpl();
+        r.delete("2.css/");
     }
 }
--- src/main/java/mvc/repository/JavaIOSkillRepositoryImpl.java
+++ src/main/java/mvc/repository/JavaIOSkillRepositoryImpl.java
@@ -2,9 +2,7 @@ package mvc.repository;
 
 import mvc.model.Skill;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -13,15 +11,15 @@ import java.util.ArrayList;
  * Created by Max Hluhov on 14.10.2018.
  */
 public class JavaIOSkillRepositoryImpl implements SkillRepository {
+    private File file = new File("src\\main\\resources\\files\\skills.txt");
     @Override
     public void save(Skill skill) throws IOException {
         String skills = Long.toString(skill.getID())
-                + "| " + skill.getName();
-        File file = new File("src\\main\\resources\\files\\skills.txt");
+                + "." + skill.getName() + "/";
         ArrayList<String> arr = readLines(file);
         arr.add(skills);
         try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write(skills + "\n");
+            fw.write(skills);
         } catch (IOException e) {
             System.out.println("IO exception: " + e);
         }
@@ -31,5 +29,10 @@ public class JavaIOSkillRepositoryImpl implements SkillRepository {
         Path filepath = filename.toPath();
         ArrayList<String> strList = (ArrayList<String>) Files.readAllLines(filepath);
         return strList;
+    }
+
+    public boolean delete(String remove) {
+
+        return true;
     }
 }
